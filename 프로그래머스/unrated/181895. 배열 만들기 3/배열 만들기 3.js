830002728228fd3a943c9cede696ca1da3fcaf4e
Hansol Kim <100466989+devdev2022@users.noreply.github.com>
2023-06-13 14:10:06 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.64 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,11 +1,5 @@
 function solution(arr, intervals) {
-    let result1 = []
-    let result2 = []
-    for(i=intervals[0][0]; i<=intervals[0][1]; i++) {
-        result1.push(arr[i])
-    }
-    for(i=intervals[1][0]; i<=intervals[1][1]; i++) {
-        result2.push(arr[i])
-    }
-    return result1.concat(result2)
+    const [[a,b],[c,d]] = intervals;
+
+    return [...arr.slice(a, b+1), ...arr.slice(c, d+1)];
 }
