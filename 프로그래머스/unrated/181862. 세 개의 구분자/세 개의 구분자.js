ece84bf935ea0a9bcdc2849295c6b15e84231231
Hansol Kim <100466989+devdev2022@users.noreply.github.com>
2023-06-25 14:46:53 +0900

    [unrated] Title: 세 개의 구분자, Time: 10.79 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,10 +1,1 @@
-function solution(myStr) {
-  const regex = /[abc]+/;
-  const result = myStr.split(regex).filter(x=> x !=="");
-  
-  if (result.length === 0) {
-    return ["EMPTY"];
-  }
-  
-  return result.filter(Boolean);
-}
+const solution=s=>s.match(/[^a-c]+/g)||['EMPTY']
