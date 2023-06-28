7af970f1cef3600f755c780ac1c68bad14c6f7b1
Hansol Kim <100466989+devdev2022@users.noreply.github.com>
2023-06-28 11:46:16 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -0,0 +1,8 @@
+function solution(x1, x2, x3, x4) {
+    if ((x1 !== false || x2 !== false) && (x3 !== false || x4 !== false)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
