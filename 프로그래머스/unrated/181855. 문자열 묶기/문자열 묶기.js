f5b6dce15c3b52d36e2c9f5e384ce02eed6e4c96
Hansol Kim <100466989+devdev2022@users.noreply.github.com>
2023-07-18 16:51:14 +0900

    [unrated] Title: 문자열 묶기, Time: 10.23 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
@@ -1,26 +1,4 @@
-let strArr = ["a","bc","d","efg","hi"]
-
 function solution(strArr) {
-  let letterLength = [];
-  let group = [];
-  let result = [];
-
-  for (let i = 0; i < strArr.length; i++) {
-    letterLength.push(strArr[i].length);
-  }
-
-  letterLength.map((num) => {
-    if (!group[num]) {
-      group[num] = [];
-    }
-    group[num].push(num);
-  });
-
-  for (let j = 0; j < group.length; j++) {
-    if (group[j]) {
-      result.push(group[j].length);
-    }
-  }
-
-  return Math.max(...result);
+    var answer = 0;
+    return answer;
 }
