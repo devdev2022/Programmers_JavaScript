9395a1b7d2089d3d3bf84eaa03c5383eff6d7b01
Hansol Kim <100466989+devdev2022@users.noreply.github.com>
2023-08-22 17:10:46 +0900

    [unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,14 @@
+function solution(ineq, eq, n, m) {
+    if(ineq==="<" && eq==="=") {
+        return n<=m? 1:0
+    }
+    if(ineq==="<" && eq==="!") {
+        return n<m? 1:0
+    }
+    if(ineq===">" && eq==="=") {
+        return n>=m? 1:0
+    }
+    if(ineq===">" && eq==="!") {
+        return n>m? 1:0
+    }
+}
