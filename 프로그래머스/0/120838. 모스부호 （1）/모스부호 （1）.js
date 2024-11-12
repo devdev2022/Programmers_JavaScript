cc81cc9de06e8dcac4ce87655ee80f969bfec01c
Hansol Kim <100466989+devdev2022@users.noreply.github.com>
2024-11-13 07:12:29 +0900

    [level 0] Title: 모스부호 (1), Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,15 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    const array = letter.split(" ")
+    let answer = []
+    for(let i=0; i<array.length; i++) {
+       answer.push(morse[array[i]])
+    }
+    return answer.join("");
+}
