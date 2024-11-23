66df6e3722fcebc2060fd5625be128df1c0e8249
Hansol Kim <100466989+devdev2022@users.noreply.github.com>
2024-11-23 18:27:44 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 19.27 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,60 +1,13 @@
 function solution(friends, gifts) {
-    let giftIndex = {}
-    friends.forEach(friend => (giftIndex[friend] = 0));
+    let count = {}
+    friends.forEach(friend => (count[friend] = 0));
     
     for (let i = 0; i < gifts.length; i++) {
         const [giver, receiver] = gifts[i].split(" "); 
-            giftIndex[giver] += 1;
-            giftIndex[receiver] -= 1;
+            count[giver] += 1;
+            count[receiver] -= 1;
     }
+    console.log(count)
+    //return Math.max(...Object.values(count))
     
-    let relation = {}
-    friends.forEach((friend)=>{
-        relation[friend] = {}
-        friends.forEach((remains)=>{
-            if(friend!==remains) {
-                relation[friend][remains] = 0; 
-            }
-        })
-    })
-    
-    let relationships = {};
-    friends.forEach(friend => {
-        relationships[friend] = {};
-        friends.forEach(otherFriend => {
-            if (friend !== otherFriend) {
-                relationships[friend][otherFriend] = 0; 
-            }
-        });
-    });
-
-    for (let i = 0; i < gifts.length; i++) {
-        const [giver, receiver] = gifts[i].split(" ");
-        relationships[giver][receiver] += 1;
-    }
-    
-
-    let nextMonthGifts = {};
-    friends.forEach(friend => (nextMonthGifts[friend] = 0));
-
-    for (let giver of friends) {
-        for (let receiver of friends) {
-            if (giver === receiver) continue;
-
-            const givenToReceiver = relationships[giver][receiver];
-            const receivedFromReceiver = relationships[receiver][giver];
-
-            if(givenToReceiver>receivedFromReceiver) {
-                nextMonthGifts[giver] +=1;
-            }
-            else if(givenToReceiver ===receivedFromReceiver||(givenToReceiver===0&&receivedFromReceiver===0)) {
-                const giverIndex = giftIndex[giver]
-                const receiverIndex = giftIndex[receiver]
-                if(giverIndex>receiverIndex) {
-                    nextMonthGifts[giver] += 1;
-                }
-            }
-        }
-    }
-    return Math.max(...Object.values(nextMonthGifts));
 }
