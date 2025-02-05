1116a46b047e2dd6cc2e73bff5ad0758d9a8744f
Hansol Kim <100466989+devdev2022@users.noreply.github.com>
2025-02-05 23:17:49 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,22 @@
+function solution(a, b) {
+    function gcd(x, y) {
+        while (y !== 0) {
+            let temp = y;
+            y = x % y;
+            x = temp;
+        }
+    return x;
+    }
+    
+    let gcdValue = gcd(a, b);
+    let denominator = b / gcdValue;
+    
+    while (denominator % 2 === 0) {
+        denominator /= 2;
+    }
+    while (denominator % 5 === 0) {
+        denominator /= 5;
+    }
+    
+    return denominator === 1 ? 1 : 2;
+}
